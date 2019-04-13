e678ed4910b55a0db32c47b3a942746c9e023b60
Max <zilsmc@gmail.com>
2019-04-13 21:22:50 +0200

    added typescript and ejs

---
+++ app/app.ts
@@ -0,0 +1,19 @@
+// app/app.ts
+import express from "express";
+import path from "path";
+
+const app = express();
+const port = 8080;
+
+// Configure Express to use ejs
+app.set( "views", path.join(__dirname, "../src/views") );
+app.set( "view engine", "ejs" );
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.listen(port, () => {
+  // tslint:disable-next-line:no-console
+  console.log(`Example app listening on ${port}`);
+});
